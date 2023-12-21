0e36236ebd300ecc582e1603dfa895ff74a09330
Sagar Kancharla <ksagar@pkrmsezdc.com>
2023-12-21 07:51:09 +0530

    initial code

---
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,39 @@
+package com.eazybytes.accounts;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservices RESt API Documentation",
+				description = "EazyBank Accounts microservices RESt API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "sagar",
+						email = "sagar.k@gmail.com",
+						url = "https://sagar.com"
+				),
+				license = @License(
+						name = "Apache2.0",
+						url = "https://sagar.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Accounts microservices RESt API Documentation",
+				url = "https://sagar.com"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+}
