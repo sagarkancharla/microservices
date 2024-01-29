322f7e062ec67e4d5ada8863210f5f6f50417486
sagarkancharla <sagarkancharla1993@gmail.com>
2024-01-29 10:25:50 +0530

    feign client

---
+++ section8/accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package com.eazybytes.accounts.controller;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+public class CustomerController {
+
+    private final ICustomerService customerService;
+
+    @Autowired
+    public CustomerController(ICustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam String mobileNumber) {
+        CustomerDetailsDto customerDetailsDto = customerService.fetchCustomerDetails(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
+    }
+
+}
